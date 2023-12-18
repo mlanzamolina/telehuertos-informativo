d4b64a292377e2dd37a9ae876c7b15e64d1ab083
mlanzamolinapb <dev.msps@gmail.com>
2023-12-18 12:17:34 -0600

    pushing to master

--- env.js
+++ env.js
@@ -1,5 +1,5 @@
 window.env = {
-  REACT_APP_BASE_URL: "https://des.sanpedrosula.hn",
+  REACT_APP_BASE_URL: "https://prb.sanpedrosula.hn",
   REACT_APP_X_API_KEY: "YXBpa2V5X21zcHNfdHJhbWl0ZXM=",
 };
 
--- src/components/ImageSlider/index.js
+++ src/components/ImageSlider/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Typography, Button, Modal } from "@mui/material";
 import Slider from "react-slick";
 import "./ImageSlider.css";
@@ -12,9 +12,11 @@ const ArrowButton = ({ direction, onClick }) => (
 function ImageSlider({ images }) {
   const [openModal, setOpenModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const handleImageClick = (imageLink) => {
+  const handleImageClick = (imageLink, index) => {
     setSelectedImage(`${window.env.REACT_APP_BASE_URL}/${imageLink}`);
+    setCurrentIndex(index);
     setOpenModal(true);
   };
 
@@ -22,12 +24,44 @@ function ImageSlider({ images }) {
     setOpenModal(false);
   };
 
+  const handleNextImage = () => {
+    const nextIndex = (currentIndex + 1) % images.length;
+    setSelectedImage(
+      `${window.env.REACT_APP_BASE_URL}/${images[nextIndex].ImageLink}`
+    );
+    setCurrentIndex(nextIndex);
+  };
+
+  const handlePrevImage = () => {
+    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+    setSelectedImage(
+      `${window.env.REACT_APP_BASE_URL}/${images[prevIndex].ImageLink}`
+    );
+    setCurrentIndex(prevIndex);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === "ArrowRight") {
+        handleNextImage();
+      } else if (event.key === "ArrowLeft") {
+        handlePrevImage();
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [currentIndex]);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: images?.length > 0 ? Math.min(images.length, 4) : 1,
+    slidesToScroll: 1,
     vertical: false,
     nextArrow: <ArrowButton direction="next" />,
     prevArrow: <ArrowButton direction="prev" />,
@@ -46,12 +80,12 @@ function ImageSlider({ images }) {
       {images.length > 0 ? (
         <Slider {...settings}>
           {images.map((image, index) => (
-            <Box key={index} sx={{ maxWidth: 200, width: "50%" }}>
+            <Box key={index}>
               <img
                 src={`${window.env.REACT_APP_BASE_URL}/${image.ImageLink}`}
                 alt={image.Foto}
                 className="thumbnail-image"
-                onClick={() => handleImageClick(image.ImageLink)}
+                onClick={() => handleImageClick(image.ImageLink, index)}
                 loading="lazy"
               />
             </Box>
@@ -66,15 +100,24 @@ function ImageSlider({ images }) {
       <Modal
         open={openModal}
         onClose={handleCloseModal}
-        sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          zIndex: 10000,
+        }}
       >
         <Box
           sx={{
             position: "relative",
             outline: "none",
-            maxWidth: "75vw",
-            maxHeight: "75vh",
-            overflow: "auto",
+            maxWidth: "75%",
+            maxHeight: "100%",
+            // maxHeight: "75%",
+            // overflow: "auto", // Retain overflow for scrolling
+            // backgroundColor: "white", // Add a background color for better visibility
+            // padding: "10px", // Add padding for spacing
+            // borderRadius: "10px", // Optional: to add rounded corners
           }}
         >
           <Button
@@ -83,7 +126,7 @@ function ImageSlider({ images }) {
               position: "absolute",
               top: 0,
               right: 0,
-              color: "black",
+              color: "#008d00",
               fontSize: "24px",
             }}
           >
@@ -92,8 +135,57 @@ function ImageSlider({ images }) {
           <img
             src={selectedImage}
             alt="Full Size"
-            style={{ maxWidth: "100%", maxHeight: "100%" }}
+            style={{
+              display: "block", // Ensure the image is a block element
+              maxWidth: "100%", // Limit width to 100% of parent
+              maxHeight: "100%", // Limit height to 100% of parent
+              objectFit: "contain", // Keep aspect ratio
+              margin: "auto", // Center image within the container
+            }}
           />
+
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <Button
+              onClick={handlePrevImage}
+              disabled={images.length <= 1}
+              sx={{
+                backgroundColor: "transparent",
+                border: "solid #008d00 1px",
+                cursor: "pointer",
+                fontSize: "32px",
+                color: "#008d00",
+                outline: "#008d00",
+                position: "absolute",
+                left: 0,
+              }}
+            >
+              ◀
+            </Button>
+            <Button
+              onClick={handleNextImage}
+              disabled={images.length <= 1}
+              sx={{
+                backgroundColor: "transparent",
+                border: "solid #008d00 1px",
+                cursor: "pointer",
+                fontSize: "32px",
+                color: "#008d00",
+                outline: "#008d00",
+                position: "absolute",
+                right: 0,
+              }}
+            >
+              ▶
+            </Button>
+          </Box>
         </Box>
       </Modal>
     </Box>
